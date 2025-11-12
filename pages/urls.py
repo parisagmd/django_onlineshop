db1cb113289f5ff5e22303793f893a261a86ce02
Mohsen Bolandi <mohsenbolandiholding@gmail.com>
2025-11-12 17:48:37 +0330

    add pages file and codes for home and aboutus page

---
+++ pages/urls.py
@@ -0,0 +1,7 @@
+from . import views
+from django.urls import path
+
+urlpatterns= [
+    path('', views.HomePageView.as_view(), name= 'home'),
+    path('aboutus/', views.AboutUsPageView.as_view(), name= 'aboutus'),
+]
